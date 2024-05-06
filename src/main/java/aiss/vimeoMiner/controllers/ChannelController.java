58825c5cfee927799e028efc4c9b0980a460b6ec
CurroAyala <f.ayaladiaz04@gmail.com>
2024-05-06 20:38:33 +0200

    Channel

---
+++ src/main/java/aiss/vimeoMiner/controllers/ChannelController.java
@@ -0,0 +1,35 @@
+package aiss.vimeoMiner.controllers;
+
+import aiss.vimeoMiner.models.channel.Channel;
+import aiss.vimeoMiner.services.ChannelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/videominer")
+public class ChannelController {
+
+    @Autowired
+    ChannelService service;
+
+    // @Autowired
+    // RestTemplate restTemplate;
+
+    // final String vimeoMinerUri = "http://localhost:8080/vimeominer/channels";
+
+
+    @GetMapping
+    public List<Channel> findAllChannels(){
+        return service.getAllChannels();
+    }
+
+    @GetMapping("/{id}")
+    public Channel findOneChannel(String id) {
+        return service.getOneChannel(id);
+    }
+
+}
--- src/main/java/aiss/vimeoMiner/models/channel/Channel.java
+++ src/main/java/aiss/vimeoMiner/models/channel/Channel.java
@@ -10,37 +10,21 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Channel {
 
+    public Channel(String id, String name, Object description, String createdTime) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.createdTime = createdTime;
+    }
+
     @JsonProperty("id")
     private String id;
-    @JsonProperty("uri")
-    private String uri;
     @JsonProperty("name")
     private String name;
     @JsonProperty("description")
     private Object description;
-    @JsonProperty("link")
-    private String link;
     @JsonProperty("created_time")
     private String createdTime;
-    @JsonProperty("modified_time")
-    private String modifiedTime;
-    @JsonProperty("user")
-    private User user;
-    @JsonProperty("tags")
-    private List<Object> tags;
-    @JsonProperty("pictures")
-    private Pictures__2 pictures;
-    @JsonProperty("header")
-    private Header header;
-    @JsonProperty("privacy")
-    private Privacy privacy;
-    @JsonProperty("categories")
-    private List<Object> categories;
-    @JsonProperty("metadata")
-    private Metadata__1 metadata;
-    @JsonProperty("resource_key")
-    private String resourceKey;
-
 
 
     // GETTERS AND SETTERS
@@ -50,15 +34,6 @@ public class Channel {
     @JsonProperty("id")
     public void setId(String id) { this.id = id; }
 
-    @JsonProperty("uri")
-    public String getUri() {
-        return uri;
-    }
-
-    @JsonProperty("uri")
-    public void setUri(String uri) {
-        this.uri = uri;
-    }
 
     @JsonProperty("name")
     public String getName() {
@@ -70,6 +45,7 @@ public class Channel {
         this.name = name;
     }
 
+
     @JsonProperty("description")
     public Object getDescription() {
         return description;
@@ -80,15 +56,6 @@ public class Channel {
         this.description = description;
     }
 
-    @JsonProperty("link")
-    public String getLink() {
-        return link;
-    }
-
-    @JsonProperty("link")
-    public void setLink(String link) {
-        this.link = link;
-    }
 
     @JsonProperty("created_time")
     public String getCreatedTime() {
@@ -100,94 +67,14 @@ public class Channel {
         this.createdTime = createdTime;
     }
 
-    @JsonProperty("modified_time")
-    public String getModifiedTime() {
-        return modifiedTime;
-    }
 
-    @JsonProperty("modified_time")
-    public void setModifiedTime(String modifiedTime) {
-        this.modifiedTime = modifiedTime;
+    @Override
+    public String toString() {
+        return "Channel{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", description=" + description +
+                ", createdTime='" + createdTime + '\'' +
+                '}';
     }
-
-    @JsonProperty("user")
-    public User getUser() {
-        return user;
-    }
-
-    @JsonProperty("user")
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    @JsonProperty("tags")
-    public List<Object> getTags() {
-        return tags;
-    }
-
-    @JsonProperty("tags")
-    public void setTags(List<Object> tags) {
-        this.tags = tags;
-    }
-
-    @JsonProperty("pictures")
-    public Pictures__2 getPictures() {
-        return pictures;
-    }
-
-    @JsonProperty("pictures")
-    public void setPictures(Pictures__2 pictures) {
-        this.pictures = pictures;
-    }
-
-    @JsonProperty("header")
-    public Header getHeader() {
-        return header;
-    }
-
-    @JsonProperty("header")
-    public void setHeader(Header header) {
-        this.header = header;
-    }
-
-    @JsonProperty("privacy")
-    public Privacy getPrivacy() {
-        return privacy;
-    }
-
-    @JsonProperty("privacy")
-    public void setPrivacy(Privacy privacy) {
-        this.privacy = privacy;
-    }
-
-    @JsonProperty("categories")
-    public List<Object> getCategories() {
-        return categories;
-    }
-
-    @JsonProperty("categories")
-    public void setCategories(List<Object> categories) {
-        this.categories = categories;
-    }
-
-    @JsonProperty("metadata")
-    public Metadata__1 getMetadata() {
-        return metadata;
-    }
-
-    @JsonProperty("metadata")
-    public void setMetadata(Metadata__1 metadata) {
-        this.metadata = metadata;
-    }
-
-    @JsonProperty("resource_key")
-    public String getResourceKey() {
-        return resourceKey;
-    }
-
-    @JsonProperty("resource_key")
-    public void setResourceKey(String resourceKey) {
-        this.resourceKey = resourceKey;
-    }
-
 }
