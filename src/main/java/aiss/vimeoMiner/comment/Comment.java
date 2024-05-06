bce1017b36cd8ad56fa339a10a1fa07619246c92
Javier Luque <javluqrui@alum.us.es>
2024-05-06 20:01:57 +0200

    Comment v1

---
+++ src/main/java/aiss/vimeoMiner/comment/Comment.java
@@ -0,0 +1,88 @@
+
+package aiss.vimeoMiner.comment;
+
+import java.util.List;
+import javax.annotation.Generated;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "total",
+    "page",
+    "per_page",
+    "paging",
+    "data"
+})
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Comment {
+
+    @JsonProperty("total")
+    private Integer total;
+    @JsonProperty("page")
+    private Integer page;
+    @JsonProperty("per_page")
+    private Integer perPage;
+    @JsonProperty("paging")
+    private Paging paging;
+    @JsonProperty("data")
+    private List<Datum> data;
+
+    @JsonProperty("total")
+    public Integer getTotal() {
+        return total;
+    }
+
+    @JsonProperty("total")
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    @JsonProperty("page")
+    public Integer getPage() {
+        return page;
+    }
+
+    @JsonProperty("page")
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    @JsonProperty("per_page")
+    public Integer getPerPage() {
+        return perPage;
+    }
+
+    @JsonProperty("per_page")
+    public void setPerPage(Integer perPage) {
+        this.perPage = perPage;
+    }
+
+    @JsonProperty("paging")
+    public Paging getPaging() {
+        return paging;
+    }
+
+    @JsonProperty("paging")
+    public void setPaging(Paging paging) {
+        this.paging = paging;
+    }
+
+    @JsonProperty("data")
+    public List<Datum> getData() {
+        return data;
+    }
+
+    @JsonProperty("data")
+    public void setData(List<Datum> data) {
+        this.data = data;
+    }
+
+
+
+}
