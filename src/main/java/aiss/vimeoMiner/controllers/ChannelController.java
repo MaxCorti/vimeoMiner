de07b47b1bd8cd65221d6ff41608a893db5debae
CurroAyala <f.ayaladiaz04@gmail.com>
2024-05-07 11:20:38 +0200

    User

--- src/main/java/aiss/vimeoMiner/controllers/ChannelController.java
+++ src/main/java/aiss/vimeoMiner/controllers/ChannelController.java
@@ -6,11 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
-@RequestMapping("/videominer")
+@RequestMapping("/videoMiner")
 public class ChannelController {
 
     @Autowired
