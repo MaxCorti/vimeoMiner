99fc6ebc135ce950abe203a4ac2f11047c28d29a
Nicolas Parrilla <nicolasparrillageniz@gmail.com>
2024-05-10 20:27:25 +0200

    ChannelController funcion post no funciona

--- src/main/java/aiss/vimeoMiner/controllers/ChannelController.java
+++ src/main/java/aiss/vimeoMiner/controllers/ChannelController.java
@@ -2,9 +2,12 @@ package aiss.vimeoMiner.controllers;
 
 import aiss.vimeoMiner.models.channel.Channel;
 import aiss.vimeoMiner.services.ChannelService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -16,18 +19,17 @@ public class ChannelController {
     ChannelService service;
     @Autowired
     RestTemplate restTemplate;
-    final String videoMinerUri = "http://localhost:8080/api/videoMiner/channels";
+    final String videoMinerUri = "http://localhost:8080/videominer/channels";
 
     @GetMapping("/{id}")
     public Channel getChannel(@PathVariable String id) {
         return service.getChannel(id);
     }
-
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{id}")
-    public Channel sendChannel(@PathVariable String id){
+    public ResponseEntity<String> sendChannel(@PathVariable String id){
         Channel canal = service.getChannel(id);
-        HttpEntity<Channel> request = new HttpEntity<>(canal);
-        ResponseEntity<Channel> response = restTemplate.exchange(videoMinerUri, HttpMethod.POST,request, Channel.class);
-        return response.getBody();
+        ResponseEntity<String> response = restTemplate.postForEntity(videoMinerUri, canal, String.class);
+        return response;
     }
 }
